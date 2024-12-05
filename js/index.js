37f16651f6b93e84ed8866280a457248e806fedf
jeas-github <jeas@outlook.com>
2024-12-05 17:17:48 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,50 @@
+
+let currentPage = 1; // Página inicial
+const usersPerPage = 12; // Quantidade de usuários por página
+
+window.onload = (event) => {
+  listarUsuarios(currentPage);
+};
+
+async function listarUsuarios(currentPage) {
+  try {
+    const response = await fetch(`https://fakestoreapi.com/users?limit=${usersPerPage}&page=${currentPage}`);
+    
+    const users = await response.json();
+
+    const usersHtml = users.map(user => {
+      return `
+        <tr>
+          <td>${user.id}</td>
+          <td>${user.name.firstname} ${user.name.lastname}</td>
+          <td>${user.email}</td>
+          <td>${user.username}</td>
+          <td>${user.phone}</td>
+        </tr>
+      `;
+    });
+
+    document.getElementById("lista-usuarios").innerHTML = usersHtml.join("");
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+// Funções para navegação
+function proximaPagina() {
+  currentPage++;
+  listarUsuarios(currentPage);
+}
+
+function paginaAnterior() {
+  if (currentPage > 1) {
+    currentPage--;
+    listarUsuarios(currentPage);
+  }
+}
+
+/*
+document.addEventListener("DOMContentLoaded", function () {
+  listarUsuarios(currentPage);
+});
+*/
